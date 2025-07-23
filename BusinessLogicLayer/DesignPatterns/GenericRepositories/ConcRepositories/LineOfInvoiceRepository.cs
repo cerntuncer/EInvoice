612a77ddf463978d9014c52295dc5195c9784fd1
Ceren Tuncer <cernttuncer@gmail.com>
2025-07-23 14:44:50 +0300

    CRUD işlemleri tamamlandı.

---
+++ BusinessLogicLayer/DesignPatterns/GenericRepositories/ConcRepositories/LineOfInvoiceRepository.cs
@@ -0,0 +1,13 @@
+﻿using DatabaseAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.DesignPatterns.GenericRepositories.ConcRepositories
+{
+    public class LineOfInvoiceRepository : GenericRepositories.BaseRepositories.BaseRepositoriesository<LineOfInvoice>
+    {
+    }
+}
---
+++ BusinessLogicLayer/DesignPatterns/GenericRepositories/ConcRepositories/ProductAndServiceRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using DatabaseAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.DesignPatterns.GenericRepositories.ConcRepositories
+{
+    public class ProductAndServiceRepository : GenericRepositories.BaseRepositories.BaseRepositoriesository<ProductAndService>
+    {
+    }
+}
