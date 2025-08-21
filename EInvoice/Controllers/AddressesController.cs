29d27902131df89bff01385d2781767b285db891
Ceren Tuncer <cernttuncer@gmail.com>
2025-08-21 17:38:17 +0300

    presentation sayfa navigation  tasarlandı

---
+++ EInvoice/Controllers/AddressesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EInvoice.Controllers
+{
+    [Authorize]
+    public class AddressesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
