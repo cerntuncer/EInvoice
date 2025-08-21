8d51b5193ddc6d5ae4f5465d9a5ae2a8242cb58e
Ceren Tuncer <cernttuncer@gmail.com>
2025-08-21 15:35:27 +0300

    _Layout.cshtml güncellemesi yapıldı

---
+++ EInvoice/Views/Shared/_LayoutPublic.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PresentationLayer.Views.Shared
+{
+    public class _LayoutPublicModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
