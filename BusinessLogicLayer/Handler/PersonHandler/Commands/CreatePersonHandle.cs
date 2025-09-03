f0e4969349a60a91f14e550fc5cc604b24e38dc8
Ceren Tuncer <cernttuncer@gmail.com>
2025-09-03 16:14:53 +0300

    müşteri/tedarikçi kısmında düzenleme yapıldı

--- BusinessLogicLayer/Handler/PersonHandler/Commands/CreatePersonHandle.cs
+++ BusinessLogicLayer/Handler/PersonHandler/Commands/CreatePersonHandle.cs
@@ -27,12 +27,18 @@ namespace BusinessLogicLayer.Handler.PersonHandler.Commands
                 message = "İsim Zorunludur";
             else if (request.Name.Length > 50)
                 message = "İsim 50 karakterden uzun olamaz.";
-            else if (request.IdentityNumber <= 0 || request.IdentityNumber.ToString().Length > 11)
-                message = "TCKN veya VKN uzunluğu hatalıdır.";
+            else
+            {
+                var identityStr = request.IdentityNumber.ToString().Trim();
+                if (request.IdentityNumber <= 0 || (identityStr.Length != 10 && identityStr.Length != 11))
+                    message = "TCKN 11 hane, VKN 10 hane olmalıdır.";
+            }
             else if (!string.IsNullOrWhiteSpace(request.TaxOffice) && request.TaxOffice.Length > 150)
                 message = "Vergi Dairesi adı uzunluğu 150 karakterden fazla olamaz";
             else if (!Enum.IsDefined(typeof(PersonType), request.Type))
                 message = "Kullanıcı Tipi Uyumlu Değildir.";
+            else if (!Enum.IsDefined(typeof(Status), request.Status))
+                message = "Durum bilgisi geçersiz.";
 
             if (message != null)
             {
